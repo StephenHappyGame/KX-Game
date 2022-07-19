25ea6e229aaa9bdea7a829c8d68ee51342996fe0
y3v4d <y3v4d@protonmail.com>
2022-07-19 17:12:25 +0200

    replaced graphics in NoticeDialog (Announcements)

--- assets/Script/UI/Notice/NoticeListDialog.js
+++ assets/Script/UI/Notice/NoticeListDialog.js
@@ -1,54 +1,25 @@
-// Learn cc.Class:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
-// Learn Attribute:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
-
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-
         listItem: cc.Prefab,
         content: cc.Node,
         noticeListRedPoint: cc.Node
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    updatePlayerInfo () {
+    updatePlayerInfo() {
         this.noticeListRedPoint.active = Global.Player.checkUnRead() !== 0;
     },
 
     start () {
         let data = Global.Player.getPy('emailArr');
-        if (!!data && data.length > 0){
+        if(!!data && data.length > 0) {
             data = JSON.parse(data);
-        }else{
+        } else{
             data = [];
         }
-        for (let i = data.length - 1; i >= 0; i --) {
+
+        for(let i = data.length - 1; i >= 0; i --) {
             let mailData = data[i];
             let item = cc.instantiate(this.listItem);
             item.parent = this.content;
@@ -59,11 +30,11 @@ cc.Class({
         Global.MessageCallback.addListener('UpdateUserInfoUI', this);
     },
 
-    onDestroy () {
+    onDestroy() {
         Global.MessageCallback.removeListener('UpdateUserInfoUI', this);
     },
 
-    messageCallbackHandler: function(router, msg) {
+    messageCallbackHandler(router, msg) {
         switch (router) {
             case 'UpdateUserInfoUI':
                 this.updatePlayerInfo();
@@ -71,13 +42,11 @@ cc.Class({
         }
     },
 
-    onBtnClk: function (event, param) {
+    onBtnClk(event, param) {
         switch (param) {
             case 'close':
                 Global.DialogManager.destroyDialog(this);
                 break;
         }
-    },
-
-    // update (dt) {},
+    }
 });
